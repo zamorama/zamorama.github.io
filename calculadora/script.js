cb76b73312c4e3010e8e2dcbabec1b72fbef69a5
zamorama <sebastianzamora2002@gmail.com>
2017-03-21 20:05:39 +0100

    Calculadora

---
+++ calculadora/script.js
@@ -0,0 +1,12 @@
+function resultadito() {
+  var ratio = document.getElementById("ratio").value;
+  var raciones = document.getElementById("raciones").value;
+  var gactual = document.getElementById("gluco_actual").value;
+  var gdeseada = document.getElementById("gluco_deseada").value;
+  var fsensibilidad = document.getElementById("factor_sensibilidad").value;
+  var resultadoUno = ratio * raciones;
+  var resultadoDos = (gactual - gdeseada) / fsensibilidad;
+  var resultadoFinal = resultadoUno + resultadoDos;
+  var insu = document.getElementById("insulina");
+  document.getElementById('insulinaP').innerHTML= resultadoFinal;
+}
